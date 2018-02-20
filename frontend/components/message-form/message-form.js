628148fb71ecd784dc9827f3bdf5ef92c2f2ce43
hsato <hsato1101@live.jp>
2018-02-20 21:57:54 +0900

    add chat

--- frontend/components/message-form/message-form.js
+++ frontend/components/message-form/message-form.js
@@ -1,1 +1,28 @@
 import "./message-form.css";
+import { sendMessage } from "../../client/chat";
+
+const form = document.querySelector(".js-message-form");
+const input = form.querySelector(".js-message-form--input");
+const submit = form.querySelector(".js-message-form--submit");
+
+function submitForm() {
+  // sendMessageを呼び出し、その結果Rubyのsend_messageメソッドが呼ばれて
+  // ActiveRecordでMessageインスタンスが作成される
+  sendMessage(input.value);
+  input.value = "";
+  input.focus();
+}
+
+// コマンドキー（またはCtrlキー）+Enterでメッセージを送信できる
+input.addEventListener("keydown", event => {
+  if (event.keyCode === 13 && event.metaKey) {
+    event.preventDefault();
+    submitForm();
+  }
+});
+
+// ボタンをクリックして送信してもよい
+submit.addEventListener("click", event => {
+  event.preventDefault();
+  submitForm();
+});
--- frontend/components/messages/messages.js
+++ frontend/components/messages/messages.js
@@ -1,2 +1,19 @@
+import { setCallback } from "../../client/chat";
 import "../message/message";
 import "./messages.css";
+
+const messages = document.querySelector(".js-messages");
+const content = messages.querySelector(".js-messages--content");
+
+function scrollToBottom() {
+  content.scrollTop = content.scrollHeight;
+}
+
+scrollToBottom();
+
+// ActionCableで新しいメッセージを1件受け取るたびに
+// このコード片を呼び出すよう`chat.js`に伝える
+setCallback(message => {
+  content.insertAdjacentHTML("beforeend", message);
+  scrollToBottom();
+});
